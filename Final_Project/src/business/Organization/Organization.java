7737a8bf158c40ffd4c9973963572486afab3f91
Parakh Mahajan <mahajan.pa@husky.neu.edu>
2016-11-26 19:16:05 -0500

    Final Project ver1.1

--- Final_Project/src/business/Organization/Organization.java
+++ Final_Project/src/business/Organization/Organization.java
@@ -5,10 +5,49 @@
  */
 package business.Organization;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author PARAKH MAHAJAN
  */
-public class Organization {
+public abstract class Organization {
+    private String name;
+    private int organizationID;
+    private static int counter;
+
+    public Organization(String name) {
+        this.name = name;
+        organizationID = counter;
+        ++counter;
+        
+        
+        
+    }
+    
+    public enum Type
+    {
+        Donor("Donor Organization"), Receptionist("Reception Organization"), Lab("Lab Organization"), Nurse("Nurse Organization"), Inventory("Inventory Organization"), Admin("Admin Organization");
+        private String value;
+        private Type(String value) {
+            this.value = value;
+        }
+        public String getValue() {
+            return value;
+        }
+
+    }
+    
+    //public abstract ArrayList<Role> getSupportedRole();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
     
 }
