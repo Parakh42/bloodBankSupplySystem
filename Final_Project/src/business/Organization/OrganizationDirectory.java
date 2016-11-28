5d72bd73bff2eb98f8ffd2fdc2cbe8706aa3d1a6
piyush sharma <sharma.pi@husky.neu.edu>
2016-11-28 18:33:33 -0500

    version 2.1

--- Final_Project/src/business/Organization/OrganizationDirectory.java
+++ Final_Project/src/business/Organization/OrganizationDirectory.java
@@ -5,10 +5,41 @@
  */
 package business.Organization;
 
+import java.util.ArrayList;
+import business.Organization.Organization.Type;
+
 /**
  *
  * @author PARAKH MAHAJAN
  */
 public class OrganizationDirectory {
+    
+    private ArrayList<Organization> organizationList;
+
+    public OrganizationDirectory() {
+        organizationList = new ArrayList();
+    }
+
+    public ArrayList<Organization> getOrganizationList() {
+        return organizationList;
+    }
+    
+    public Organization createOrganization(Type type){
+        Organization organization = null;
+        if (type.getValue().equals(Type.Donor.getValue())){
+            organization = new DonorOrganization();
+            organizationList.add(organization);
+        }
+        else if (type.getValue().equals(Type.Reception.getValue())){
+            organization = new ReceptionOrganization();
+            organizationList.add(organization);
+        }
+        else if (type.getValue().equals(Type.Reception.getValue())){
+            organization = new ReceptionOrganization();
+            organizationList.add(organization);
+        }
+        
+        return organization;
+    }
     
 }
